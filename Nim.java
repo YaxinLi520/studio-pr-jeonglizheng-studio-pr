f13850e74b06be2784be8afd9af446c51cf2b0fe
YaxinLi520 <122591160+YaxinLi520@users.noreply.github.com>
2023-04-20 13:24:55 -0500

    Add files via upload

--- Nim.java
+++ Nim.java
@@ -2,62 +2,73 @@ package assignment2;
 
 import java.util.Scanner;
 
+/**
+ * 
+ * @author Yaxin Li
+ *
+ */
 public class Nim {
 
-	public static void main(String[] args)
-	{
-	Scanner in = new Scanner(System.in);
-	
-	System.out.println("Initial Number of Sticks: ");
-	int initial = in.nextInt();
-	int i = 0;
-	int k = 0;
-	while (initial>0)
-	{
-		System.out.println("How Many Sticks Do You Want to Remove?: ");
-		int value = in.nextInt();
-		if (value == initial && i == 0 || value > 2 || initial<=0)
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Please enter the number of initial sticks: ");
+		int initialSticks = in.nextInt();
+		
+		int beginSticks = initialSticks;
+		int endSticks = initialSticks;
+		int round = 0;
+		int human = 0;
+		int computer = 0;
+		
+		while (beginSticks > 0)
 		{
-			System.out.println("Illegal Move.");
+			System.out.println("Please enter the number of sticks you'll remove (1 or 2): ");
+			human = in.nextInt();
+			while ((human != 2 && human != 1) || (human > beginSticks))
+			{
+				System.out.println("Please enter a valid number (1 or 2) / a smaller number: ");
+				human = in.nextInt();
+			}
+			
+			endSticks -= human;
+			
+			System.out.println("Round " + round + ": " + beginSticks + " at start human takes "
+			+ human + ", so " + endSticks + " remain.");
+			
+			beginSticks = endSticks;
+			round ++;
+			
+			if (beginSticks > 0)
+			{
+				computer = (int) Math.round(Math.random() + 1);
+				
+				while (computer > beginSticks)
+				{
+					computer = (int) Math.round(Math.random() + 1);
+				}
+				
+				endSticks -= computer;
+				
+				System.out.println("Round " + round + ": " + beginSticks + " at start computer takes "
+						+ computer + ", so " + endSticks + " remain.");
+				
+				beginSticks = endSticks;
+				round ++;
+				
+				if  (beginSticks == 0)
+				{
+					System.out.println("The computer wins / you lose!");
+				}
+			}
+			else
+			{
+				System.out.println("The computer loses / you win!");
+			}
+			
 		}
-		else
-		{	
-			System.out.println("Round "+i+": "+initial+" at start, "+"human takes "+value+", so "+(initial-value)+" remain.");
-			i++;
-			initial-=value;
-			k=initial;
-			if (initial==0)
-			{
-			initial=0;
-			System.out.println("You Win!");
-			}
-			if (initial == 1)
-			{
-			int comp = initial;
-			System.out.println("Round "+i+": "+initial+" at start, "+"computer takes "+comp+", so "+(initial-comp)+" remain.");
-			System.out.println("Computer wins");
-			initial=0;
-			}
-			if (initial == 1 && i % 2 == 0)
-			{
-			System.out.println("Round "+i+": "+initial+" at start, "+"human takes "+value+", so "+(initial-value)+" remain.");
-			initial-=value;
-			initial=0;
-			}
-			else if (initial >= 2)
-			{	
-			int comp = (int)((Math.random()*2)+1);
-			System.out.println("Round "+i+": "+initial+" at start, "+"computer takes "+comp+", so "+(initial-comp)+" remain.");
-			initial-=comp;
-			k = initial;
-			i++;
-			if (initial == 0)
-			{
-				System.out.println("The computer wins");
-			}
-			}
+		
 	}
-			//The best strategy for the computer is to have the human decide upon the value three and then choose the last one.
-	}		
-	}
+
 }
