5f50d85e574e99f23552dd0f15480f6a0c28dc30
Judy-Zheng <77987092+Judy-yzheng@users.noreply.github.com>
2023-04-20 13:28:00 -0500

    Add files via upload

--- Nim.java
+++ Nim.java
@@ -2,73 +2,93 @@ package assignment2;
 
 import java.util.Scanner;
 
-/**
- * 
- * @author Yaxin Li
- *
- */
 public class Nim {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
-		
-		System.out.println("Please enter the number of initial sticks: ");
+		System.out.println("sticks you want to start: ");
 		int initialSticks = in.nextInt();
+		int i = 0; //count times
 		
-		int beginSticks = initialSticks;
-		int endSticks = initialSticks;
-		int round = 0;
-		int human = 0;
-		int computer = 0;
 		
-		while (beginSticks > 0)
+		while (initialSticks > 0)
 		{
-			System.out.println("Please enter the number of sticks you'll remove (1 or 2): ");
-			human = in.nextInt();
-			while ((human != 2 && human != 1) || (human > beginSticks))
+			//human move 0,2,4
+			if (i%2 ==0)
 			{
-				System.out.println("Please enter a valid number (1 or 2) / a smaller number: ");
-				human = in.nextInt();
-			}
-			
-			endSticks -= human;
-			
-			System.out.println("Round " + round + ": " + beginSticks + " at start human takes "
-			+ human + ", so " + endSticks + " remain.");
-			
-			beginSticks = endSticks;
-			round ++;
-			
-			if (beginSticks > 0)
-			{
-				computer = (int) Math.round(Math.random() + 1);
+				//enter number to move
+				System.out.print("How many sticks you want to move: ");
+				double humanMove = in.nextDouble();
 				
-				while (computer > beginSticks)
+				//if it's double
+				boolean ifDouble = humanMove == (int) humanMove;
+				if(!ifDouble)
 				{
-					computer = (int) Math.round(Math.random() + 1);
+					System.out.println("Please enter an integer, you can only enter 1 or 2!");
+				}
+				//over than 2 or less than 1
+				else if (humanMove > 2)
+				{
+					System.out.println("You moved too many, you can only choose to move 1 or 2 sticks!");
+					//System.out.print("How many sticks you want to move: ");
+					//int humanMove1 = in.nextInt();
+				}
+				else if(humanMove <= 0)
+				{
+					System.out.println("You should move atleast 1, you can only choose to move 1 or 2 sticks!");
+					//System.out.print("How many sticks you want to move: ");
+					//int humanMove1 = in.nextInt();
+				}
+				//normal move
+				else
+				{
+					initialSticks = (int) (initialSticks - humanMove);
+					
+					System.out.println("Round " + i + ": start human takes " + (int) humanMove + ", so " + initialSticks + " remain");
+					
+					if (initialSticks == 0)
+					{
+						System.out.println("The human wins / computer lose!");
+					}
+					i++;
 				}
 				
-				endSticks -= computer;
 				
-				System.out.println("Round " + round + ": " + beginSticks + " at start computer takes "
-						+ computer + ", so " + endSticks + " remain.");
-				
-				beginSticks = endSticks;
-				round ++;
-				
-				if  (beginSticks == 0)
+			}
+			//computer move
+			if (i%2 != 0 && initialSticks != 0)
+			{
+				int computerMove;
+				//normal move
+				if (initialSticks > 1)
+				{
+					computerMove = (int) (Math.random() * 2 + 1);
+					initialSticks = initialSticks - computerMove;
+					System.out.println("Round " + i + ": start computer takes " + computerMove + ", so " + initialSticks + " remain");
+				}
+				//final move, only have 1 stick
+				else if (initialSticks == 1)
+				{
+					computerMove = 1;
+					initialSticks = initialSticks - computerMove;
+					System.out.println("Round " + i + ": start computer takes " + computerMove + ", so " + initialSticks + " remain");
+					
+				}
+				//end game
+				if (initialSticks == 0)
 				{
 					System.out.println("The computer wins / you lose!");
 				}
+				i++;
 			}
-			else
-			{
-				System.out.println("The computer loses / you win!");
-			}
+			//i++;
+			
+			
+			
 			
 		}
-		
+
 	}
 
 }
